36a875dbbfbc80570afd1f6481bedc634d8085da
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-15 22:08:14 -0500

    Agregar la funcion de agregar los nombres de amigos a la lista y mostrarlos

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de
+// programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    amigos.push(inputAmigo.value);
+    mostrarAmigos(inputAmigo.value);
+
+    inputAmigo.value = '';
+}
+
+function mostrarAmigos(inputAmigo){
+    let lista = document.getElementById("listaAmigos");
+
+    let nuevo = document.createElement("li");
+
+    nuevo.textContent = inputAmigo;
+
+    lista.appendChild(nuevo);
+}
